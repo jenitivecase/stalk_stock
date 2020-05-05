31f6239e9e39425f984943769c189ebe4f7573ed
jenitivecase <jbrussow@gmail.com>
2020-05-05 05:39:06 -0400

    words words words

--- weekly_analysis.R
+++ weekly_analysis.R
@@ -5,3 +5,21 @@ sapply(c("gsheet", "tidyverse", "DT", "knitr"), load_packages)
 
 stalks <- as.data.frame(gsheet2tbl("docs.google.com/spreadsheets/d/1SVxFEGjCGhj3q6NQB1cJNf5CLeFTo9giG79HdOqJXgo/",
                                    sheetid = "selling"))
+
+##### GENERAL PROJECT STAGES ###################################################
+#
+# 1. MVP: hand-coded algorithm to identify pattern. >= 3 data points required.
+# 2. loess regression lines?
+# 3. time series analysis
+# 4. ML time series analysis
+#
+##### QUEsTIONS ################################################################
+#
+# - how to handle missing data?
+# - are there really four patterns?
+# - are the four pattern labels put forth by the internet really real?
+#
+################################################################################
+
+
+##### AN ATTEMPT AT WRITING DECISION RULES #####################################
